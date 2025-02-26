c96ea2cd5e13f7eb2bcc15bac295fc57575eccc4
admxj <admxj@126.com>
2025-02-27 01:28:30 +0800

    :sparkles: router 改造

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,21 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import copyPlugin from 'rollup-plugin-copy'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: './ws-test/',
+    base: './',
     plugins: [
         vue(),
         vueDevTools(),
+        copyPlugin({
+            targets: [
+                { src: 'static/js/*', dest: 'dist/static/js' },
+            ],
+            hook: 'writeBundle',
+        }),
     ],
     resolve: {
         alias: {
